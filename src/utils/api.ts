59205a4ebc4df4f85a96b4bb2296b4b3bf9c638e
Anunay Nayan <anunayan123@gmail.com>
2025-07-10 18:33:51 +0530

    Added API endpoint

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "https://pixisphere-api-k3af.onrender.com/api",
 });
 
 export const getPhotographers = () => API.get("/photographers");
